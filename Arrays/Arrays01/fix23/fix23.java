3376e5f2fc70a1a715d714c38817671f5bf68cdc
singhrishabhj <singhrishabhj19@gmail.com>
2024-06-10 00:24:26 +0530

    Arrays_01,10jun24

---
+++ Arrays/Arrays01/fix23/fix23.java
@@ -0,0 +1,13 @@
+//fix23
+
+public int[] fix23(int[] nums) {
+  
+  if(nums[0]==2 && nums[1]==3){
+    nums[1]=0;
+    
+  }
+  else if(nums[1]==2 && nums[2]==3){
+    nums[2]=0;
+  }
+  return nums;
+}
