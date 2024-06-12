88c0accca9e31d66877abf69f469224929918363
singhrishabhj <singhrishabhj19@gmail.com>
2024-06-12 21:24:00 +0530

    Correct Solution Updated

--- Arrays/Arrays01/maxEnds3/maxEnds3.java
+++ Arrays/Arrays01/maxEnds3/maxEnds3.java
@@ -2,16 +2,14 @@
 
 public int[] maxEnd3(int[] nums) {
   int max;
-  if(nums[0]>nums[1] && nums[0]>nums[1]){
+  if(nums[0]>nums[2] && nums[0]>nums[2]){
     max=nums[0];
-  } else if (nums[1]>nums[2] && nums[1]>nums[0]){
-    max=nums[1];
   }else 
-   max=nums[2]; 
+    max=nums[2];
    
    nums[0]=max; //assigning the max value to arrays
    nums[1]=max;
    nums[2]=max;
    
-   return nums; 
+   return nums;
 }
