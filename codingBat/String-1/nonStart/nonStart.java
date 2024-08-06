abdea9e083a1b827d08edf1f567704be0c88f601
singhrishabhj <singhrishabhj19@gmail.com>
2024-08-06 21:35:13 +0530

    UPdated String1

---
+++ codingBat/String-1/nonStart/nonStart.java
@@ -0,0 +1,10 @@
+//nonStart
+
+public String nonStart(String a, String b) {
+  
+  String firstString = a.substring(1);
+  String secondString = b.substring(1);
+   
+   
+   return firstString+secondString;
+}
