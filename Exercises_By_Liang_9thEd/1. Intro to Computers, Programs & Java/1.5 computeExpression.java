e6503c483461e29951178e60b73b98c96e44a8c4
RISHABH SINGH <152042945+singhrishabhj@users.noreply.github.com>
2024-10-29 00:29:08 +0530

    Question 1.5 computeExpression.java

---
+++ Exercises_By_Liang_9thEd/1. Intro to Computers, Programs & Java/1.5 computeExpression.java
@@ -0,0 +1,13 @@
+/*
+1.5 ( Compute Expression)
+    Write the program that displays the result of 
+         7.5 x 2.5 - 1.5 x 3
+     =   -------------------
+              35.5 - 2.5
+*/
+
+public class ComputeExpression{
+  public static void main (String[] args){
+    System.out.println( ( 7.5 * 2.5 - 1.5 * 3 ) / ( 35.5 - 2.5 ) ); 
+  }
+}
