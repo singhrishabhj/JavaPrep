d5f5dc463d6f70ef98e44698c520caa3470a23b7
RISHABH SINGH <152042945+singhrishabhj@users.noreply.github.com>
2024-10-29 00:35:21 +0530

    Question 1.6 MultiplicationOfSeries.java

---
+++ Exercises_By_Liang_9thEd/1. Intro to Computers, Programs & Java/1.6 MultiplicationOfSeries.java
@@ -0,0 +1,13 @@
+/*
+1.6 ( Multiplication of a Series )
+    Write a program that displays the results of :
+    1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 x 9 
+*/
+
+
+
+public class MultiplicationOfSeries{
+  public static void main (String [] args){
+    System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9);
+  }
+}
