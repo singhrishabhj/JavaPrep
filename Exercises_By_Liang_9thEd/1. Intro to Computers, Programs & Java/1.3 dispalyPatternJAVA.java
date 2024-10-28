99d97407fed159c7c71cb33d2f419e2dace09436
RISHABH SINGH <152042945+singhrishabhj@users.noreply.github.com>
2024-10-29 00:17:49 +0530

    Question 1.3
    IN this we have to display the java as a pattern .

---
+++ Exercises_By_Liang_9thEd/1. Intro to Computers, Programs & Java/1.3 dispalyPatternJAVA.java
@@ -0,0 +1,16 @@
+/*
+1.3 Display Patterns give below
+     j       A       V      V       A
+     j     A   A      V    V      A    A
+j    j   A A A A A     V  V     A A A A A
+ j j    A         A      V     A         A
+*/
+
+public class PatternJAVA{
+public static void main(String[] args){
+  System.out.println("     J       A       V      V       A");
+  System.out.println("     J     A   A      V    V      A    A");
+  System.out.println("J    J   A A A A A     V  V     A A A A A");
+  System.out.println(" J J    A         A      V     A         A");
+}
+}
