4db6cace240c4701222905e3530c22f9ca8cc3dc
RISHABH SINGH <152042945+singhrishabhj@users.noreply.github.com>
2024-10-29 00:01:36 +0530

    Create 1.1 Display two Messages.java

---
+++ Exercises_By_Liang_9thEd/1. Intro to Computers, Programs & Java/1.1 Display two Messages.java
@@ -0,0 +1,11 @@
+/* 
+1.1 ( Display two messages)
+Write a program that displays "Welcome to Java Programming , and you have succeeded.
+*/
+
+public class Welcome{
+public static void main (String [] args){
+  System.out.println("Welcome to Java Programming");
+  System.out.println("You have Suceeded");
+}
+}
