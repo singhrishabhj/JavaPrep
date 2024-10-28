06d9ba9c96e923a13bcdf9f5e9a8163ec20d811c
RISHABH SINGH <152042945+singhrishabhj@users.noreply.github.com>
2024-10-29 00:23:19 +0530

    Question 1.4 DisplayTable.java

---
+++ Exercises_By_Liang_9thEd/1. Intro to Computers, Programs & Java/1.4 DisplayTable.java
@@ -0,0 +1,19 @@
+/*
+1.4 (Print a Table)
+    Write a program that displays the following table:
+    a  a*2  a*3
+    1   2    3    
+    2   4    6
+    3   6    9
+    4   8    12
+*/
+
+public class PrintTable{
+ public static void main(String[] args){
+   System.out.println("a  a*2  a*3");
+   System.out.println("1   2    3");
+   System.out.println("2   4    6");
+   System.out.println("3   6    9");
+   System.out.println("4   8    12");
+}
+}
